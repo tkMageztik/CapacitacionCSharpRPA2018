b7a5a4244a61832d397e73a50b4d677d58bf3e07
tkmageztik <jujex_90@hotmail.com>
2018-03-08 21:52:23 -0500

    se agregó proyecto .net core, con entity framework.

---
+++ SlnCapacitacionCSharpRPA2018/ASPNETCoreWebApp/Data/DbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using ASPNETCoreWebApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWebApp.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Alumno.Any())
+            {
+                return;
+            }
+
+            var alumnos = new Alumno[]
+            {
+                new Alumno{ Nombre = "Juan", Apellido="Ruiz de Castilla"}
+            };
+
+            foreach (Alumno a in alumnos)
+            {
+                context.Alumno.Add(a);
+            }
+
+            //var tipDocs = new TipoDocumentoViewModel[]
+            //{
+            //    new TipoDocumentoViewModel{ TipDoc = "DNI", DesDoc="DNI"},
+            //    new TipoDocumentoViewModel{ TipDoc = "Pasaporte", DesDoc="Pasaporte"},
+            //    new TipoDocumentoViewModel{ TipDoc="CE", DesDoc="Carné de extranjería"}
+            //};
+            //foreach (TipoDocumentoViewModel s in tipDocs)
+            //{
+            //    context.TipoDocumentoViewModel.Add(s);
+            //}
+
+
+            context.SaveChanges();
+        }
+
+    }
+
+}
---
+++ SlnCapacitacionCSharpRPA2018/ASPNETCoreWebApp/ViewModel/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWebApp.ViewModel
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
